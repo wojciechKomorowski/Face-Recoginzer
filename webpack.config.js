fb087ab48e58a25a1b10228a157beb8c50e1a68f
Wojciech Komorowski <komora89@gmail.com>
2017-12-16 22:35:52 +0100

    Webpack.config file.

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+module.exports = {
+    entry: "./js/app.jsx",
+    output: { path: path.resolve("dist"), filename: "./out.js" },
+    watch: true,
+    module: {
+        loaders: [ {
+            test: /\.jsx$/, exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: { presets: ['es2015', 'stage-2', 'react'] }
+        } ] 
+    }
+}
