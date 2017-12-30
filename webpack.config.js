92b34400c0382fe4242d06882c63771f1096b4a7
Wojciech Komorowski <komora89@gmail.com>
2017-12-30 15:56:04 +0100

    Changed dist folder location.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 module.exports = {
-    entry: "./js/app.jsx",
-    output: { path: path.resolve("dist"), filename: "./out.js" },
+    entry: "./public/js/app.jsx",
+    output: { path: path.resolve(__dirname, "./public/dist"), filename: "./out.js" },
     watch: true,
     module: {
         loaders: [ {
